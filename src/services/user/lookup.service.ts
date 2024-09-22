b18624c28050ce5efc561fa09aeb71032ffec200
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-09-22 23:29:01 +0200

    mise à jour de l'organisation

--- src/services/user/lookup.service.ts
+++
@@ -1,8 +0,0 @@
-export const lookupEmail = async (email: string) => {
-  return await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/lookup`, {
-    method: "POST",
-    body: JSON.stringify({
-      email,
-    }),
-  });
-};
