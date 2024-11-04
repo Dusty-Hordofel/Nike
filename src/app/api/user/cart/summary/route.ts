60d55403e4fd3f0bfe72cad9cf4620d99ad16212
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-05 00:05:27 +0100

    Update route.ts

--- src/app/api/user/cart/summary/route.ts
+++ src/app/api/user/cart/summary/route.ts
@@ -1,4 +1,3 @@
-// import { isValidObjectId } from "@/lib/utils";
 import { isValidObjectId } from "mongoose";
 import Cart from "@/models/cart.model";
 import User from "@/models/user.model";
@@ -6,68 +5,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { auth } from "@/auth";
 
-export const GET = auth(async (req) => {
-  console.log("🚀 ~ GET ~ req:", req.auth);
-
-  return Response.json({ message: "User has been updated" });
+export const GET = auth(async (req: Request) => {
+  return NextResponse.json(
+    { success: true, error: false, message: "Get all carts" },
+    { status: 200 }
+  );
 });
-
-export async function POST(
-  req: NextRequest,
-  { params: { userId } }: { params: { userId: string } }
-) {
-  try {
-    if (!isValidObjectId(userId)) {
-      return new NextResponse(
-        JSON.stringify({
-          success: false,
-          error: true,
-          message: "Unauthorized",
-        }),
-        { status: 400 }
-      );
-    }
-
-    const dbUser = await User.findOne({
-      _id: userId,
-    });
-
-    if (!dbUser) {
-      return new NextResponse(
-        JSON.stringify({
-          success: true,
-          error: false,
-          message: "Unauthorized User",
-        }),
-        { status: 400 }
-      );
-    }
-
-    const cart = await Cart.findOne({ user: dbUser._id });
-    console.log("🚀 ~ cart:", cart);
-    if (!cart) {
-      return new NextResponse(
-        JSON.stringify({
-          success: true,
-          error: false,
-          message: "cart not found",
-        }),
-        {
-          status: 400,
-        }
-      );
-    }
-
-    console.log("🚀 ~ getCart ~ cart:", cart);
-
-    return new NextResponse(
-      JSON.stringify({ success: true, error: false, cart }),
-      {
-        status: 200,
-      }
-    );
-  } catch (error) {
-    console.log("🚀 ~ getCart ~ error:", error);
-    return { error: "An error occurred while loading cart items" };
-  }
-}
