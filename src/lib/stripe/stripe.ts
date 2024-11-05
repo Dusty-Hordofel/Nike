8ea7b86e2b3f5cb4c59c3352ea5d73c0bf051c4c
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-05 01:31:11 +0100

    Update stripe.ts

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -1,17 +1,22 @@
+"use server";
 import Stripe from "stripe";
 
 // if (!process.env.STRIPE_SECRET_KEY) {
 //   throw new Error("STRIPE_SECRET_KEY is not defined in environment variables");
 // }
 
-export const stripe = new Stripe(
-  "sk_test_51NZaglAJF88lxObTvUS00OhlIuefFG16bfLlymCGOiEhA9A7kAjp9Z8d18PM1yZtYJUHfsWJn4lL4TsPpqPsO5BS00BE5U4Fy4" ??
-    "",
-  {
-    apiVersion: "2024-06-20",
-    typescript: true,
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-06-20",
+  typescript: true,
+});
+
+// export const stripe = new Stripe(
+//   "sk_test_51NZaglAJF88lxObTvUS00OhlIuefFG16bfLlymCGOiEhA9A7kAjp9Z8d18PM1yZtYJUHfsWJn4lL4TsPpqPsO5BS00BE5U4Fy4",
+//   {
+//     apiVersion: "2024-06-20",
+//     typescript: true,
+//   }
+// );
 // export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
 //   apiVersion: "2024-06-20",
 //   typescript: true,
