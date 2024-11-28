449a04267a54789048784cfe31e11a302956646a
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-28 01:51:25 +0100

    106. Disclaimer

--- README.md
+++ README.md
@@ -1294,6 +1294,8 @@ $  npm install stripe @stripe/stripe-js @stripe/react-stripe-js
 
 ### 105. Dashboard 2
 
+### 106. Disclaimer
+
 <!-- TODO: Work on  CheckoutSectionHeader and CheckoutHeader component's name -->
 
 ## External Links
---
+++ src/components/ui/disclaimer.tsx
@@ -0,0 +1,19 @@
+const Disclaimer = () => {
+  return (
+    <p className="bg-black-200 text-white px-[50px] py-5">
+      This website is a personal project created as part of my developer
+      portfolio to showcase my web development skills. Since{" "}
+      <span className="font-medium">Nike</span> is such a significant part of
+      our lives, I wanted to explore and understand how their website is built.
+      This project is in no way affiliated with, endorsed, or sponsored by{" "}
+      <span className="font-medium">Nike</span>. All images, trademarks, and
+      related elements associated with <span className="font-medium">Nike</span>{" "}
+      remain the property of their respective owners. If any copyright
+      infringement is identified, please contact me at{" "}
+      <span className="font-medium">hordofel@gmail.com</span> for immediate
+      resolution.
+    </p>
+  );
+};
+
+export default Disclaimer;
