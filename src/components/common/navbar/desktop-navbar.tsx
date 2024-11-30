ddf953b44a3fdd20953507cc1f87e4d89fdec4d7
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-30 01:56:22 +0100

    auth link

--- src/components/common/navbar/desktop-navbar.tsx
+++ src/components/common/navbar/desktop-navbar.tsx
@@ -5,14 +5,15 @@ import SearchInput from "./search/SearchInput";
 import NavLinks from "./navigation/nav-links";
 import { menuLinks } from "@/assets/data/menu-links";
 import Link from "next/link";
-import { useCart } from "@/context/cart/cart.context";
 import { CartState } from "@/context/cart/cart.reducer";
+import { useCurrentUser } from "@/hooks/user/auth/use-current-user.hook";
 
 interface DesktopNavbarProps {
   expand: boolean;
   cartState: CartState;
 }
 const DesktopNavbar = ({ expand, cartState }: DesktopNavbarProps) => {
+  const user = useCurrentUser();
   return (
     <header className="relative z-10 max-w-[1920px] h-16 bg-white hidden min-[960px]:block">
       <div className="grid grid-cols-12 px-12 h-full items-center overflow-hidden">
@@ -37,6 +38,28 @@ const DesktopNavbar = ({ expand, cartState }: DesktopNavbarProps) => {
 
             {!expand && (
               <>
+                {!user && (
+                  <Link
+                    className="rounded-full p-[6px] hover:bg-gray-200 "
+                    href={`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`}
+                  >
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      viewBox="0 0 24 24"
+                      role="img"
+                      width="24px"
+                      height="24px"
+                      fill="none"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"
+                      ></path>
+                    </svg>
+                  </Link>
+                )}
                 <Link
                   className="link-icon-hover"
                   href={`${process.env.NEXT_PUBLIC_BASE_URL}/favourites`}
